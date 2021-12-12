186e142a6349944fcaba7464abbb027ce28903b4
Gergo Varga <varga.gergo0509@gmail.com>
2021-12-12 14:12:31 +0000

    Started working on score

--- Blackjack2/Score.cpp
+++ Blackjack2/Score.cpp
@@ -1,1 +1,28 @@
 #include "Score.h"
+
+int Score::SetScore()
+{
+    m_score = 0;
+    return m_score;
+}
+
+int Score::AddScore()
+{
+    m_newScore = m_value + m_score;
+    return m_newScore;
+}
+
+int Score::GetScore()
+{
+    return m_newScore;
+}
+
+int Score::NewScore(int newScore)
+{
+    return m_newScore;
+}
+
+Cards::Value Score::GetValue(int value)
+{
+    return Cards::Value();
+}
--- Blackjack2/Score.h
+++ Blackjack2/Score.h
@@ -1,6 +1,23 @@
 #pragma once
+#include "Cards.h"
+
 class Score
 {
+public:
+
+	int SetScore();
+	int AddScore();
+
+	int GetScore();
+	int NewScore(int newScore);
+
+	Cards::Value GetValue(int value);
+
+private:
+
+	int m_score;
+	int m_newScore;
+	int m_value;
 
 };
 
